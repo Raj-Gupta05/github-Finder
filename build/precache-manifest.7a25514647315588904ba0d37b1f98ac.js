553d23e38e4086222551e9624336b2d10304781f
Raj-Gupta05 <rg3722549@gmail.com>
2023-07-25 22:35:14 +0530

    final

---
+++ build/precache-manifest.7a25514647315588904ba0d37b1f98ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe621da74f306115c9d34f8b448b8aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb2c8a4b64328ffe810a",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a30e6b66c6906818d1c5",
+    "url": "/static/js/2.82aebd89.chunk.js"
+  },
+  {
+    "revision": "eb2c8a4b64328ffe810a",
+    "url": "/static/js/main.dae259ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
